2e684ae65fa11eb5f748953421fb8e3e546c86ee
MikaRyklief <131654288+MikaRyklief@users.noreply.github.com>
2025-09-10 13:31:45 +0200

    Add project files.

---
+++ PROG7312_Part1_POE_ST10318273/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROG7312_Part1_POE_ST10318273.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
